dee8adf61942b3eb609726452d98ba0a89d3a32b
hsiaoluw <hsiaoluw@usc.edu>
2016-11-12 09:53:25 -0800

    Geo,Ray
    Geometry, Ray
    also change light, material part for our application

--- Material.h
+++ Material.h
@@ -1,3 +1,5 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
 // I'm unsure how our design will look (whether these will all be in one file or several files),
 // but if in several files the color class will need to be included here - Kayla
 
@@ -10,16 +12,21 @@ public:
     GzColor Kd;   // diffuse color
     GzColor Ka;   // ambient color
     GzColor Ks;   // specular color
+	float   s;   //(N*L)^s
                   // add texture component later
                   // add refractive index later
+	float reflectiveness;
 
-    GzMaterial(GzColor d, GzColor a, GzColor s);
+    GzMaterial(GzColor kd, GzColor ka, GzColor ks, float S,  float r);
 };
 
 // Constructor makes a new material with the given diffuse, ambient, and specular properties
-GzMaterial::GzMaterial(GzColor d, GzColor a, GzColor s)
+GzMaterial::GzMaterial(GzColor kd, GzColor ka, GzColor ks, float S,  float r)
 {
-    Kd = d;
-    Ka = a;
-    Ks = s;
+    Kd = kd;
+    Ka = ka;
+    Ks = ks;
+	s  =  S;    // for (N*L)^s , specular
+	reflectiveness = r; // reflect ray amplitue
 }
+#endif

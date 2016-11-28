fa975ed8cbffc974e1fa70012904b3cda2922fc6
Elelynsalt <tpiao@usc.edu>
2016-11-27 18:09:29 -0800

    gai

--- GzMatrix.cpp
+++ GzMatrix.cpp
@@ -66,7 +66,7 @@ GzMatrix GzMatrix::inverseTranspose() const
     detA = M[0][0]*M[1][1]*M[2][2] + M[1][0]*M[2][1]*M[0][2]
     + M[2][0]*M[0][1]*M[1][2] - M[0][0]*M[2][1]*M[1][2]
     - M[2][0]*M[1][1]*M[0][2] - M[1][0]*M[0][1]*M[2][2];
-    if (detA == 0.0f) return M;
+    if (detA == 0.0f) return temp;
     temp.M[0][0] = M[1][1]*M[2][2] - M[1][2]*M[2][1];
     temp.M[0][1] = M[1][2]*M[2][0] - M[1][0]*M[2][2];
     temp.M[0][2] = M[1][0]*M[2][1] - M[1][1]*M[2][0];

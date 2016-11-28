45ee6a5f5d7a1c059f1cd8f355afaca831346ec4
Elelynsalt <tpiao@usc.edu>
2016-11-27 17:47:25 -0800

    GzMatrix v1.2

--- GzMatrix.cpp
+++ GzMatrix.cpp
@@ -22,37 +22,37 @@ GzMatrix::GzMatrix()
 }
 GzMatrix::GzMatrix(const GzVector3 &v, int type) : GzMatrix()
 {
-    if (type == 0)
+    if (type == TRANSLATION)
     {
         M[0][3] = v.x;
         M[1][3] = v.y;
         M[2][3] = v.z;
     }
-    else if (type == 1)
+    else if (type == SCALING)
     {
         M[0][0] = v.x;
         M[1][1] = v.y;
         M[2][2] = v.z;
     }
 }
-GzMatrix::GzMatrix(float arc, int axis) : GzMatrix()
+GzMatrix::GzMatrix(float angle, int axis) : GzMatrix()
 {
-    float rad = arc * PI / 180;
-    if (axis == 0)
+    float rad = angle * PI / 180;
+    if (axis == X)
     {
         M[1][1] = cos(rad);
         M[2][2] = cos(rad);
         M[2][1] = -sin(rad);
         M[1][2] = sin(rad);
     }
-    if (axis == 1)
+    if (axis == Y)
     {
         M[0][0] = cos(rad);
         M[3][3] = cos(rad);
         M[2][0] = sin(rad);
         M[0][2] = -sin(rad);
     }
-    if (axis == 2)
+    if (axis == Z)
     {
         M[0][0] = cos(rad);
         M[1][1] = cos(rad);
@@ -67,32 +67,32 @@ GzMatrix GzMatrix::inverseTranspose() const
     detA = M[0][0]*M[1][1]*M[2][2] + M[1][0]*M[2][1]*M[0][2]
     + M[2][0]*M[0][1]*M[1][2] - M[0][0]*M[2][1]*M[1][2]
     - M[2][0]*M[1][1]*M[0][2] - M[1][0]*M[0][1]*M[2][2];
-    if (deta == 0.0f) return M;
-    temp[0][0] = M[1][1]*M[2][2] - M[1][2]*M[2][1];
-    temp[1][0] = M[1][2]*M[2][0] - M[1][0]*M[2][2];
-    temp[2][0] = M[1][0]*M[2][1] - M[1][1]*M[2][0];
-    temp[0][1] = M[0][2]*M[2][1] - M[0][1]*M[2][2];
-    temp[1][1] = M[0][0]*M[2][2] - M[0][2]*M[2][0];
-    temp[2][1] = M[0][1]*M[2][0] - M[0][0]*M[2][1];
-    temp[0][2] = M[0][1]*M[1][2] - M[0][2]*M[1][1];
-    temp[1][2] = M[0][2]*M[1][0] - M[0][0]*M[1][2];
-    temp[2][2] = M[0][0]*M[1][1] - M[0][1]*M[1][0];
+    if (detA == 0.0f) return M;
+    temp.M[0][0] = M[1][1]*M[2][2] - M[1][2]*M[2][1];
+    temp.M[1][0] = M[1][2]*M[2][0] - M[1][0]*M[2][2];
+    temp.M[2][0] = M[1][0]*M[2][1] - M[1][1]*M[2][0];
+    temp.M[0][1] = M[0][2]*M[2][1] - M[0][1]*M[2][2];
+    temp.M[1][1] = M[0][0]*M[2][2] - M[0][2]*M[2][0];
+    temp.M[2][1] = M[0][1]*M[2][0] - M[0][0]*M[2][1];
+    temp.M[0][2] = M[0][1]*M[1][2] - M[0][2]*M[1][1];
+    temp.M[1][2] = M[0][2]*M[1][0] - M[0][0]*M[1][2];
+    temp.M[2][2] = M[0][0]*M[1][1] - M[0][1]*M[1][0];
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            temp[i][j] = temp[i][j] / detA;
+            temp.M[i][j] = temp.M[i][j] / detA;
         }
     }
     return temp;
 }
 GzVector3 operator*(const GzMatrix &m, const GzVector3 &v)
 {
-    flaot a, b, c, d;
-    a = m[0][0]*v.x + m[0][1]*v.y + m[0][2]*v.z + m[0][3];
-    b = m[1][0]*v.x + m[1][1]*v.y + m[1][2]*v.z + m[1][3];
-    c = m[2][0]*v.x + m[2][1]*v.y + m[2][2]*v.z + m[2][3];
-    d = m[3][0]*v.x + m[3][1]*v.y + m[3][2]*v.z + m[3][3];
+    float a, b, c, d;
+    a = m.M[0][0]*v.x + m.M[0][1]*v.y + m.M[0][2]*v.z + m.M[0][3];
+    b = m.M[1][0]*v.x + m.M[1][1]*v.y + m.M[1][2]*v.z + m.M[1][3];
+    c = m.M[2][0]*v.x + m.M[2][1]*v.y + m.M[2][2]*v.z + m.M[2][3];
+    d = m.M[3][0]*v.x + m.M[3][1]*v.y + m.M[3][2]*v.z + m.M[3][3];
     if (d == 0.0f) return GzVector3();
     return GzVector3(a/d, b/d, c/d);
 }
@@ -105,7 +105,7 @@ GzMatrix operator*(const GzMatrix &m1, const GzMatrix &m2)
         {
             for (int k = 0; k < 3; k++)
             {
-                result[i][j] += m1[i][k] * m2[k][j];
+                result.M[i][j] += m1.M[i][k] * m2.M[k][j];
             }
         }
     }
